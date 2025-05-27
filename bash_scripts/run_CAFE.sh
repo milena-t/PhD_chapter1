f589fc56482ac5c352f60a85b14c940057723eee
milena-t <milena.trabert@t-online.de>
2025-05-27 21:46:23 +0200

    add nw tree species name parsing

--- bash_scripts/run_CAFE.sh
+++ bash_scripts/run_CAFE.sh
@@ -2,8 +2,12 @@
 
 NATIVE_INFILE=/Users/milena/work/chapter1_final_postprocessing/orthofinder_native/CAFE_input_native_from_N0.tsv
 TREE_NATIVE=/Users/milena/work/chapter1_final_postprocessing/orthofinder_native/SpeciesTree_native.nw
-cafe5 -i $NATIVE_INFILE -t $TREE_NATIVE -c 2 -o /Users/milena/work/chapter1_final_postprocessing/CAFE_native > CAFE_out_native.log &
+TREE_NAT_MOD="${TREE_NATIVE%.*}_only_species_names.nw"
+sed 's/_native_isoform_filtered_proteins//g' $TREE_NATIVE > $TREE_NAT_MOD
+cafe5 -i $NATIVE_INFILE -t $TREE_NAT_MOD -c 2 -o /Users/milena/work/chapter1_final_postprocessing/CAFE_native > CAFE_out_native.log &
 
 ORTHODB_INFILE=/Users/milena/work/chapter1_final_postprocessing/orthofinder_uniform/CAFE_input_orthoDB_from_N0.tsv
 TREE_ORTHODB=/Users/milena/work/chapter1_final_postprocessing/orthofinder_uniform/SpeciesTree_uniform.nw
+TREE_ODB_MOD="${TREE_ORTHODB%.*}_only_species_names.nw"
+sed 's/_filtered_proteinfasta_TE_filtered//g' $TREE_ORTHODB > $TREE_ODB_MOD
 cafe5 -i $ORTHODB_INFILE -t $TREE_ORTHODB -c 2 -o /Users/milena/work/chapter1_final_postprocessing/CAFE_uniform > CAFE_out_uniform.log &
