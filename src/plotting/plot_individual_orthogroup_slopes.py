d15ad10d8440f8c9b2796b1556b5ab19a4fedd4a
milena-t <milena.trabert@t-online.de>
2025-06-17 14:56:44 +0200

    remove option to plot inclines as lines

--- src/plotting/plot_individual_orthogroup_slopes.py
+++ src/plotting/plot_individual_orthogroup_slopes.py
@@ -25,30 +25,10 @@ def plot_slopes(GF_sizes_dict, species_list, exp_dict, x_label, filename = "sig_
     else:
         filename = f"{color_category}_{filename}"
 
-    fs = 15 # set font size
-    # plot each column in the dataframe as a line in the same plot thorugh a for-loop
-    fig = plt.figure(figsize=(10,8))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.tick_params(axis='both', which='major', labelsize=fs)
-    
-    ylab="mean number of orthogroup members"
-    # get a list of lists with [native, orthoDB] number of gene families per species
-
-    legend_labels = []
-    ncol_legend = 0
-
-    colors = {
-        "native" : "#b82946", # red
-        "native_unsignificant" : "#DE6880", #light red
-        "orthoDB": "#F2933A", # orange
-        "orthoDB_unsignificant" : "#F6B679", # light orange
-        "background" : "#838383" # grey
-    }
-    
     inclines = {}
     intercepts = {}
     OG_sizes = {}
-
+    
     for orthogroup, GF_sizes in tqdm(GF_sizes_dict.items()):
         GF_sizes_vec = [GF_sizes[species] if species in GF_sizes else 0 for species in species_list ]
         x_axis_vec = [exp_dict[species] for species in species_list]
@@ -64,26 +44,32 @@ def plot_slopes(GF_sizes_dict, species_list, exp_dict, x_label, filename = "sig_
     percentile_lower = np.percentile(inclines_list, q = 100-percentile)
     print(f"max incline: {max(inclines_list):.5f},\t\tmin_incline: {min(inclines_list):.5f} \n95th percentile: {percentile_upper:.5f},\t5th percentile: {percentile_lower:.5f}")
 
-    not_plotted_lines = 0
+
+    fs = 20 # set font size
+    
+    ylab="mean number of orthogroup members"
+    # get a list of lists with [native, orthoDB] number of gene families per species
+
+    colors = {
+        "native" : "#b82946", # red
+        "native_unsignificant" : "#DE6880", #light red
+        "orthoDB": "#F2933A", # orange
+        "orthoDB_unsignificant" : "#F6B679", # light orange
+        "background" : "#838383" # grey
+    }
+    
+
+    
+
+    not_significant_lines = 0
     for orthogroup, incline in tqdm(inclines.items()):
         if incline > percentile_upper or incline < percentile_lower:
             # print(f"{orthogroup} : incline {incline:.4f}")
-            ax.plot(x_axis_vec, [incline*x_value+intercepts[orthogroup] for x_value in x_axis_vec], color = colors[color_category], linewidth = 1)
+            # ax.plot(x_axis_vec, [incline*x_value+intercepts[orthogroup] for x_value in x_axis_vec], color = colors[color_category], linewidth = 1)
+            pass
         else:
-            not_plotted_lines+=1
+            not_significant_lines+=1
             # ax.plot(x_axis_vec, [incline*x_value+intercepts[orthogroup] for x_value in x_axis_vec], color = colors["background"], linewidth = 0.5)
-
-    ylab = f"gene family size (only {100-percentile}th and {percentile}th percentile shown, \n{len(inclines)-not_plotted_lines} of {len(inclines)} orthogroups)"
-    ax.set_ylabel(ylab, fontsize = fs)
-    ax.set_xlabel(x_label, fontsize = fs)
-    title_ = x_label.split(" in")[0]
-    title = f"regression lines of gene family size vs. {title_}"
-    plt.title(title, fontsize=fs*1.2)
-
-    plt.tight_layout()
-    plt.savefig(filename, dpi = 300, transparent = True)
-    print(f"{len(inclines)-not_plotted_lines} of {len(inclines)} orthogroups not in upper or lower {percentile}th percentile were not plotted")
-    print("Figure with regression lines saved in the current working directory directory as: "+filename)
 
     ################################
     # make scatterplot of regression inclines and orthogroup sizes
@@ -97,17 +83,23 @@ def plot_slopes(GF_sizes_dict, species_list, exp_dict, x_label, filename = "sig_
     colors_list = [colors[color_category] if incline > percentile_upper or incline < percentile_lower else colors["background"] for incline in inclines_list]
 
     ax.scatter(OG_sizes_list, inclines_list, color = colors_list)
-    ylab = f"regression slope (color by {100-percentile}th and {percentile}th percentile, \n{len(inclines)-not_plotted_lines} of {len(inclines)} orthogroups)"
+    ax.axhline(percentile_upper, linestyle='--', color = colors["background"])
+    ax.text(0.5*max(OG_sizes_list), percentile_upper, f'{percentile_upper:.3f}', color='r', va='top', ha="center", transform=ax.get_yaxis_transform())
+    ax.axhline(percentile_lower, linestyle='--', color = colors["background"])
+    ax.text(0.5*max(OG_sizes_list), percentile_lower, f'{percentile_lower:.3f}', color='r', va='top', ha="center", transform=ax.get_yaxis_transform())
+    
+    ylab = f"regression slope (color by {100-percentile}th and {percentile}th percentile, \n{len(inclines)-not_significant_lines} of {len(inclines)} orthogroups)"
     ax.set_ylabel(ylab, fontsize = fs)
     ax.set_xlabel("orthogroup size", fontsize = fs)
-    title = f"regression slopes of gene family size vs. {title_}"
+    title_ = x_label.split(" in")[0]
+    title = f"regression slopes of \ngene family size vs. {title_}"
     plt.title(title, fontsize=fs*1.2)
 
-    plt.tight_layout()
+    #plt.tight_layout()
 
     filename = filename.split(".png")[0]
     filename = f"{filename}_vs_OG_size.png"
-    plt.savefig(filename, dpi = 300, transparent = True)
+    plt.savefig(filename, dpi = 300, transparent = False, bbox_inches='tight')
     print("Figure with slopes and OG sizes saved in the current working directory directory as: "+filename)
     
     return inclines
@@ -157,14 +149,14 @@ if __name__ == "__main__":
                         "C_maculatus" : 73.49
                         }
     
+    if False:
+        print(f"\n\tnative")
+        native_dict_lists = OGs.parse_orthogroups_dict(orthogroups_native_filepath)
+        native_dict = OGs.get_GF_sizes(native_dict_lists)
+        native_sig_list, native_cafe_list = OGs.get_sig_orthogroups(sig_native)
 
-    print(f"\n\tnative")
-    native_dict_lists = OGs.parse_orthogroups_dict(orthogroups_native_filepath)
-    native_dict = OGs.get_GF_sizes(native_dict_lists)
-    native_sig_list, native_cafe_list = OGs.get_sig_orthogroups(sig_native)
-
-    plot_slopes(GF_sizes_dict=native_dict, species_list = species_names, exp_dict=genome_sizes_dict, x_label = "Genome size in Mb", color_category="native", filename = f"{data_dir}sig_OGs_vs_GS_inclines.png")
-    plot_slopes(GF_sizes_dict=native_dict, species_list = species_names, exp_dict=repeat_percentages, x_label = "Repeat content in percent", color_category="native", filename = f"{data_dir}sig_OGs_vs_reps_inclines.png")
+        plot_slopes(GF_sizes_dict=native_dict, species_list = species_names, exp_dict=genome_sizes_dict, x_label = "Genome size in Mb", color_category="native", filename = f"{data_dir}sig_OGs_vs_GS_inclines.png")
+        plot_slopes(GF_sizes_dict=native_dict, species_list = species_names, exp_dict=repeat_percentages, x_label = "Repeat content in percent", color_category="native", filename = f"{data_dir}sig_OGs_vs_reps_inclines.png")
 
 
     print(f"\n\torthoDB")
