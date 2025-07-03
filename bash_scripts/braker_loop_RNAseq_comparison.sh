36bd1eb925c829d59b15a680daa9ed7f6d460f97
milena-t <milena.trabert@t-online.de>
2025-07-03 17:01:16 +0200

    fix directory of assembly

--- bash_scripts/braker_loop_RNAseq_comparison.sh
+++ bash_scripts/braker_loop_RNAseq_comparison.sh
@@ -2,10 +2,11 @@
 
 ORTHODB_ARTHROPODA=/proj/naiss2023-6-65/Milena/annotation_pipeline/annotation_protein_data/OrthoDB_Arthropoda_v11.fa
 # Lu2024
-cd /proj/naiss2023-6-65/Milena/annotation_pipeline/RNA_proteinrefs_annotation/annotation_species/superscaffolded_comparison/Cmac_Lu2024_simple
+LU_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded_comparison/Cmac_Lu2024_simple
+cd $LU_DIR
 
 sbatch --job-name="Cmac_scaff_Lu2024" --output="Cmac_scaff_Lu2024.out" -t 5-00:00:00 \
-/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_Lu2024 assembly_genomic.fna.masked \
+/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_Lu2024 ${LU_DIR}/assembly_genomic.fna.masked \
 $ORTHODB_ARTHROPODA SRR27182120,SRR27182119
 
 
