44b80790b6fcb9ab2521b5cc399c833c94ab6056
milena-t <milena.trabert@t-online.de>
2025-07-03 16:42:09 +0200

    annotation comparison cmac superscaffolded

---
+++ bash_scripts/braker_loop_RNAseq_comparison.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ORTHODB_ARTHROPODA=/proj/naiss2023-6-65/Milena/annotation_pipeline/annotation_protein_data/OrthoDB_Arthropoda_v11.fa
+# Lu2024
+cd /proj/naiss2023-6-65/Milena/annotation_pipeline/RNA_proteinrefs_annotation/annotation_species/superscaffolded_comparison/Cmac_Lu2024_simple
+
+sbatch --job-name="Cmac_scaff_Lu2024" --output="Cmac_scaff_Lu2024.out" -t 5-00:00:00 \
+/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_Lu2024 assembly_genomic.fna.masked \
+$ORTHODB_ARTHROPODA SRR27182120,SRR27182119
+
+
+
+# cd /proj/naiss2023-6-65/Milena/annotation_pipeline/RNA_proteinrefs_annotation/annotation_species/superscaffolded_comparison/Cmac_SI_diverse
+# cd /proj/naiss2023-6-65/Milena/annotation_pipeline/RNA_proteinrefs_annotation/annotation_species/superscaffolded_comparison/Cmac_Lome_diverse
