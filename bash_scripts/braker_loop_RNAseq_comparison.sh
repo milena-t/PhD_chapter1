f6c6d5de2b9b59ae1a458f7df186f4991e707b22
milena-t <milena.trabert@t-online.de>
2025-07-04 11:32:57 +0200

    replace exp numbers with run numbers

--- bash_scripts/braker_loop_RNAseq_comparison.sh
+++ bash_scripts/braker_loop_RNAseq_comparison.sh
@@ -16,11 +16,11 @@ SI_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded
 cd $SI_DIR
 sbatch --job-name="Cmac_scaff_SI_DE" --output="Cmac_scaff_SI_diverse.out" -t 5-00:00:00 \
 /proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_SI_diverse ${SI_DIR}/assembly_genomic.fna.masked \
-$ORTHODB_ARTHROPODA SRX1541697,SRX1541641,SRX1541696,SRX1541643,SRX1541689,SRX1541646,SRX1541693,SRX1541649,SRX1541651,SRX1541640,SRX1541639
+$ORTHODB_ARTHROPODA SRR3113423,SRR3113340,SRR3113422,SRR3113342,SRR3113415,SRR3113345,SRR3113419,SRR3113348,SRR3113351,SRR3113339,SRR3113337
 
 # Lome line (diverse set for differential expression from Kaufmann 2024, same population as assembly)
 LOME_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded_comparison/Cmac_Lome_diverse
 cd $LOME_DIR
 sbatch --job-name="Cmac_scaff_LOME_DE" --output="Cmac_scaff_LOME_diverse.out" -t 5-00:00:00 \
 /proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_SI_diverse ${LOME_DIR}/assembly_genomic.fna.masked \
-$ORTHODB_ARTHROPODA ERX11759692,ERX11759666,ERX11759673,ERX11759707,ERX11759712,ERX11759732,ERX11759689,ERX11759708,ERX11759670,ERX11759684
+$ORTHODB_ARTHROPODA ERR12383274,ERR12383248,ERR12383255,ERR12383289,ERR12383294,ERR12383314,ERR12383271,ERR12383290,ERR12383252,ERR12383266
