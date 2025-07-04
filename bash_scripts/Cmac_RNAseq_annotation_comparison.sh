41bddf9b85a58b4f763dedc022ab0ccdc53ab5d4
milena-t <milena.trabert@t-online.de>
2025-07-04 11:04:27 +0200

    add SRR numbers for annot comparison

---
+++ bash_scripts/Cmac_RNAseq_annotation_comparison.sh
@@ -0,0 +1,158 @@
+## RNAseq datasets for annotations
+
+## Kaufmann2024 Lome lines for DE analysis:
+# https://www.ebi.ac.uk/ena/browser/view/PRJEB70958
+    ERX11759666
+    ERX11759707
+    ERX11759732
+    ERX11759708
+    ERX11759684
+    ERX11759714
+    ERX11759725
+    ERX11759723
+    ERX11759715
+    ERX11759685
+    ERX11759681
+    ERX11759730
+    ERX11759722
+    ERX11759731
+    ERX11759679
+    ERX11759678
+    ERX11759704
+    ERX11759690
+    ERX11759692
+    ERX11759673
+    ERX11759712
+    ERX11759689
+    ERX11759670
+    ERX11759727
+    ERX11759668
+    ERX11759687
+    ERX11759665
+    ERX11759694
+    ERX11759674
+    ERX11759713
+    ERX11759705
+    ERX11759691
+    ERX11759669
+    ERX11759734
+    ERX11759735
+    ERX11759701
+    ERX11759702
+    ERX11759682
+    ERX11759672
+    ERX11759686
+    ERX11759709
+    ERX11759728
+    ERX11759683
+    ERX11759667
+    ERX11759718
+    ERX11759698
+    ERX11759724
+    ERX11759677
+    ERX11759700
+    ERX11759711
+    ERX11759688
+    ERX11759671
+    ERX11759703
+    ERX11759695
+    ERX11759696
+    ERX11759716
+    ERX11759733
+    ERX11759729
+    ERX11759726
+    ERX11759699
+    ERX11759706
+    ERX11759680
+    ERX11759721
+    ERX11759710
+    ERX11759719
+    ERX11759720
+    ERX11759675
+    ERX11759697
+    ERX11759717
+    ERX11759676
+    ERX11759693
+    ERX11733017
+#
+# Since the samples are not further identified than species, I have randomly selected a subsample of the same size as the SI sample
+# 
+# ERX11759692,ERX11759666,ERX11759673,ERX11759707,ERX11759712,ERX11759732,ERX11759689,ERX11759708,ERX11759670,ERX11759684
+
+
+
+## Sayadi2016 South india lines all samples for DE analysis:
+    SRX1541697 (adult male Head+thorax)
+    SRX1541692 (adult male Head+thorax)
+    SRX1541675 (adult male Head+thorax)
+    SRX1541668 (adult male Head+thorax)
+    SRX1541667 (adult male Head+thorax)
+    SRX1541666 (adult male Head+thorax)
+    SRX1541662 (adult male Head+thorax)
+    SRX1541661 (adult male Head+thorax)
+    SRX1541660 (adult male Head+thorax)
+    SRX1541659 (adult male Head+thorax)
+    SRX1541642 (adult male Head+thorax)
+    SRX1541641 (adult male Head+thorax)
+    SRX1541696 (adult male abdomen)
+    SRX1541695 (adult male abdomen)
+    SRX1541694 (adult male abdomen)
+    SRX1541673 (adult male abdomen)
+    SRX1541672 (adult male abdomen)
+    SRX1541671 (adult male abdomen)
+    SRX1541665 (adult male abdomen)
+    SRX1541664 (adult male abdomen)
+    SRX1541663 (adult male abdomen)
+    SRX1541645 (adult male abdomen)
+    SRX1541644 (adult male abdomen)
+    SRX1541643 (adult male abdomen)
+    SRX1541689 (adult female Head+thorax)
+    SRX1541688 (adult female Head+thorax)
+    SRX1541681 (adult female Head+thorax)
+    SRX1541677 (adult female Head+thorax)
+    SRX1541676 (adult female Head+thorax)
+    SRX1541674 (adult female Head+thorax)
+    SRX1541655 (adult female Head+thorax)
+    SRX1541654 (adult female Head+thorax)
+    SRX1541653 (adult female Head+thorax)
+    SRX1541648 (adult female Head+thorax)
+    SRX1541647 (adult female Head+thorax)
+    SRX1541646 (adult female Head+thorax)
+    SRX1541693 (adult female abdomen)
+    SRX1541691 (adult female abdomen)
+    SRX1541690 (adult female abdomen)
+    SRX1541678 (adult female abdomen)
+    SRX1541680 (adult female abdomen)
+    SRX1541679 (adult female abdomen)
+    SRX1541658 (adult female abdomen)
+    SRX1541657 (adult female abdomen)
+    SRX1541656 (adult female abdomen)
+    SRX1541652 (adult female abdomen)
+    SRX1541650 (adult female abdomen)
+    SRX1541649 (adult female abdomen)
+    SRX1541651 (Mix of adults whole body parts)
+    SRX1541640 (Pupae whole body parts)
+    SRX1541639 (Larvae whole body parts)
+#
+# I have used two representative of each category to cut down on compute hours but still represent the diversity of the dataset: 
+    SRX1541697 (adult male Head+thorax)
+    SRX1541641
+    SRX1541696 (adult male abdomen)
+    SRX1541643
+    SRX1541689 (adult female Head+thorax)
+    SRX1541646
+    SRX1541693 (adult female abdomen)
+    SRX1541649
+    SRX1541651 (Mix of adults whole body parts)
+    SRX1541640 (Pupae whole body parts)
+    SRX1541639 (Larvae whole body parts)
+#
+# SRX1541697,SRX1541641,SRX1541696,SRX1541643,SRX1541689,SRX1541646,SRX1541693,SRX1541649,SRX1541651,SRX1541640,SRX1541639
+
+
+
+# Lu2024 (no info about population but presumeably different from the Lome one that our assembly is from)
+    sex: SRR27182120
+    developmental stage: SRR27182119
+# 
+# SRR27182120,SRR27182119
--- bash_scripts/braker_loop_RNAseq_comparison.sh
+++ bash_scripts/braker_loop_RNAseq_comparison.sh
@@ -16,11 +16,11 @@ SI_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded
 cd $SI_DIR
 sbatch --job-name="Cmac_scaff_SI_DE" --output="Cmac_scaff_SI_diverse.out" -t 5-00:00:00 \
 /proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_SI_diverse ${SI_DIR}/assembly_genomic.fna.masked \
-$ORTHODB_ARTHROPODA ## TODO get right numbers SRR27182120,SRR27182119
+$ORTHODB_ARTHROPODA SRX1541697,SRX1541641,SRX1541696,SRX1541643,SRX1541689,SRX1541646,SRX1541693,SRX1541649,SRX1541651,SRX1541640,SRX1541639
 
 # Lome line (diverse set for differential expression from Kaufmann 2024, same population as assembly)
-SI_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded_comparison/Cmac_Lome_diverse
-cd $SI_DIR
-sbatch --job-name="Cmac_scaff_SI_DE" --output="Cmac_scaff_SI_diverse.out" -t 5-00:00:00 \
-/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_SI_diverse ${SI_DIR}/assembly_genomic.fna.masked \
-$ORTHODB_ARTHROPODA ## TODO get right numbers SRR27182120,SRR27182119
+LOME_DIR=/proj/naiss2023-6-65/Milena/annotation_pipeline/Cmac_Lome_superscaffolded_comparison/Cmac_Lome_diverse
+cd $LOME_DIR
+sbatch --job-name="Cmac_scaff_LOME_DE" --output="Cmac_scaff_LOME_diverse.out" -t 5-00:00:00 \
+/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh Cmac_SI_diverse ${LOME_DIR}/assembly_genomic.fna.masked \
+$ORTHODB_ARTHROPODA ERX11759692,ERX11759666,ERX11759673,ERX11759707,ERX11759712,ERX11759732,ERX11759689,ERX11759708,ERX11759670,ERX11759684
