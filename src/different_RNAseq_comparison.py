3cb713d11b998b72a2d3029bc3e204b76189bdb2
milena-t <milena.trabert@t-online.de>
2025-07-07 14:14:05 +0200

    add single exon stats

---
+++ src/different_RNAseq_comparison.py
@@ -0,0 +1,11 @@
+"""
+compare annotation statistics for the three versions of the annotation on the superscaffolded Cmac Lome assembly
+"""
+
+def filepaths_SE_stats():
+    SE_stats_dict = {
+        "Lome_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/Lome_single_exon_stats.txt",
+        "Lu_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/Lu_single_exon_stats.txt",
+        "SI_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/SI_single_exon_stats.txt",
+    }
+    return SE_stats_dict
