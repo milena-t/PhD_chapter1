193c232179ef7bf2ba793c9cc2748507698a20b2
milena-t <milena.trabert@t-online.de>
2025-07-07 14:27:19 +0200

    modify for easier parsing

--- src/different_RNAseq_comparison.py
+++ src/different_RNAseq_comparison.py
@@ -7,5 +7,20 @@ def filepaths_SE_stats():
         "Lome_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/Lome_single_exon_stats.txt",
         "Lu_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/Lu_single_exon_stats.txt",
         "SI_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/SI_single_exon_stats.txt",
+        "Kaufmann_single_exon_stats" : "/Users/miltr339/work/PhD_code/PhD_chapter1/data/Lome_RNA_annot_comparison/Kaufmann_nonsuperscaffoleded.txt"
     }
     return SE_stats_dict
+
+
+def parse_SE_stats(filepath:str):
+    """
+    parse the single exon stats outfiles 
+    """
+    transcripts_string = "total number of transcripts"
+    SE_string = "no. single exon transcripts"
+    ME_String = "no. multi exon transcripts"
+    SE_length_String = "single-exon transcripts with average length"
+    ME_length_String = "multi-exon transcripts with average length"
+
+if __name__ == "__main__":
+    SE_stats_paths = filepaths_SE_stats()
