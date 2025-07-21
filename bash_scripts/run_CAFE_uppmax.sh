74e8b6b13a71df3f6c53605068e9eff3b5c4a2f2
milena-t <milena.trabert@t-online.de>
2025-07-21 17:49:13 +0200

    fix typo

--- bash_scripts/run_CAFE_uppmax.sh
+++ bash_scripts/run_CAFE_uppmax.sh
@@ -2,17 +2,20 @@
 
 #SBATCH -A uppmax2025-2-148
 #SBATCH -p core
-#SBATCH -n 20
+#SBATCH -n 16
 #SBATCH -t 10:00:00
-#SBATCH -J CAFE_%j
-#SBATCH -o CAFE_%j.log
+#SBATCH -J CAFE_uniform
+#SBATCH -o CAFE_uniform.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
 CAFE_path=/proj/naiss2023-6-65/Milena/CAFE/CAFE5-5.0/bin/cafe5
 
-ORTHODB_INFILE=/Users/milena/work/chapter1_final_postprocessing/orthofinder_uniform/CAFE_input_orthoDB_from_N0.tsv
-TREE_ORTHODB=/Users/milena/work/chapter1_final_postprocessing/orthofinder_uniform/SpeciesTree_uniform.nw
-TREE_ODB_MOD="${TREE_ORTHODB%.*}_only_species_names.nw"
-sed 's/_filtered_proteinfasta_TE_filtered//g' $TREE_ORTHODB > $TREE_ODB_MOD
-CAFE_path -i $ORTHODB_INFILE -t $TREE_ORTHODB -c 2 -o /Users/milena/work/chapter1_final_postprocessing/CAFE_uniform > CAFE_out_uniform.log &
+ORTHODB_INFILE=/proj/naiss2023-6-65/Milena/CAFE/updated_input/CAFE_input_orthoDB_from_N0_filtered.tsv
+TREE_ULTRAMETRIC=/proj/naiss2023-6-65/Milena/CAFE/updated_input/native_orthofinder_tree_ultrametrick.nw
+
+# may need to remove the filename suffix of the tree leaf names
+# TREE_ODB_MOD="${TREE_ORTHODB%.*}_only_species_names.nw"
+# sed 's/_filtered_proteinfasta_TE_filtered//g' $TREE_ORTHODB > $TREE_ODB_MOD
+
+$CAFE_path -i $ORTHODB_INFILE -t $TREE_ULTRAMETRIC -c 16 -o /proj/naiss2023-6-65/Milena/CAFE
