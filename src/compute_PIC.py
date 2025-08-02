e429854da1f1d645d0cad6bdcfa903aba49224ed
milena-t <milena.trabert@t-online.de>
2025-08-02 16:05:02 +0200

    computation algorithm implement

--- src/compute_PIC.py
+++ src/compute_PIC.py
@@ -56,25 +56,71 @@ def test_all_leaves_have_traits(tree, trait_values):
         if node.is_leaf() == False:
             continue
         elif node.is_leaf() and node.name in trait_names:
-            continue 
+            # node.add_feature(value = trait_values[node.name])
+            node.add_feature("value", trait_values[node.name])
         else: # node is leaf and node.name not in trait_names
             node.delete()
             print(f"  !!! {node.name} is a tree leaf with no assigned trait --> delete from tree")
-    
     return tree
+
         
 
-def calculate_PIC(tree_path:str, trait_values:dict[str,float]):
+def calculate_PIC(tree_path:str, trait_values:dict[str,float], verbose = False, max_iterations = 0):
     """
     calculate phylogenetically independent contrasts of trait values in respect to the tree
     the trait values are in a dictionary where the keys correspond to the leaf names in the tree!
     """
+    if max_iterations == 0:
+        max_iterations = len(trait_values)**2
+
     tree = Tree(tree_path)
 
     ##  prepare data: remove trait values if they're not in the tree and remove tree leaves if they don't have trait values
     trait_values, discarded_species_list = match_trait_dict_to_tree_leaves(tree, trait_values)
     tree = test_all_leaves_have_traits(tree, trait_values)
 
+    PICs = []
+    ## Traverse through tree from the leaves up
+    node_number = 1
+
+    while len(PICs) < len(trait_values)-1:
+        for node in tree.traverse("postorder"):
+
+            if len(node.children)== 2:
+                children = node.children
+                child1 = children[0]
+                child2 = children[1]
+
+                if child1.is_leaf() and child2.is_leaf():
+
+                    # calculate standardized contrast between two leaves
+                    c = child1.value - child2.value
+                    sum_dist = child1.dist + child2.dist
+                    s = c/sum_dist
+
+                    PICs.append(s)
+
+                    # prune leaves and update MRCA to become a new leaf
+                    node_value = (child1.value/child1.dist + child2.value/child2.dist) / (1/child1.dist + 1/child2.dist)
+                    node_dist = node.dist + child1.dist*child2.dist/(child1.dist+child2.dist)
+
+                    # node.add_feature(value = node_value)
+                    node.add_feature("value", node_value)
+                    node.dist = node_dist
+                    node.name = f"anc({child1.name},{child2.name})"
+
+                    if verbose:
+                        print(f"s: {s:.4}\t for {child1.name} and {child2.name}")
+
+                    node.remove_child(child1)
+                    node.remove_child(child2)
+                    # print(f" after deleting children is node {node} a leaf: {node.is_leaf()}")
+
+        node_number += 1
+        if node_number == max_iterations:
+            raise RuntimeError(f"time out while tree traversing, {node_number} loops")
+    
+    return PICs
     
     
 """
@@ -106,10 +152,14 @@ def compute_pic(node):
     return x_combined, v_combined + node.dist
 
 
+
 """
 
 
 if __name__ == "__main__":
+
+    ## check against R calculations:
+    # /Box Sync/code/annotation_pipeline/repeatmasking_eval/gene_numbers_stats.Rmd
     
     ultrametric_tree = "/Users/miltr339/work/PhD_code/PhD_chapter1/data/orthofinder_native/SpeciesTree_native_ultrametric.nw"
     test_OG = {
@@ -145,12 +195,24 @@ if __name__ == "__main__":
                         "C_maculatus" : 1202 
                         }	
     
-    # calculate_PIC(tree_path=ultrametric_tree, trait_values=test_OG)
-    	
-    newick_str = "((Human:0.2,Chimp:0.2):0.1,Gorilla:0.3);"
-    traits = {"Human": 5.1,"Chimp": 4.8,"Gorilla": 6.3}
+    # coleoptera_PICs = calculate_PIC(tree_path=ultrametric_tree, trait_values=genome_sizes_dict, verbose=True)
+    # coleoptera_PICs.sort()
+    # print(coleoptera_PICs)
+    # 	
+    # newick_str = "((Human:0.2,Chimp:0.2):0.1,Gorilla:0.3);"
+    # traits = {"Human": 5.1,"Chimp": 4.8,"Gorilla": 6.3}
+    # R results: -1.9091883  0.4743416
 
-    calculate_PIC(newick_str, traits)
+    primates_tree2 = "((((Homo:0.21,Pongo:0.21):0.28,Macaca:0.49):0.13,Ateles:0.62):0.38,Galago:1.00);"
+    primates_traits_a2 = {"Homo":4.09434, "Pongo":3.61092,"Macaca":2.37024, "Ateles":2.02815, "Galago":-1.46968}
+    # R results: 0.7459333 1.1929263 1.5847416 3.3583189 
+    primates_traits_b2 = {"Homo":4.74493,"Pongo":3.33220,"Macaca":3.36730, "Ateles":2.89037, "Galago":2.30259}
+    # R results: 0.7176125 0.8678969 0.8970604 2.1798897 
+
+    primate_PICs = calculate_PIC(primates_tree2, primates_traits_a2)
+    primate_PICs.sort()
+    print(primate_PICs)
+    
     	
     	
     	
