10caae7b3b8a81f0bb05192f31c7c343ed69f7b6
milena-t <milena.trabert@t-online.de>
2025-10-15 14:12:35 +0200

    update transcript surroundings 2

--- bash_scripts/run_ReVis.sh
+++ bash_scripts/run_ReVis.sh
@@ -2,80 +2,47 @@
 
 ## Run ReVis in transcript surroundings mode (https://github.com/milena-t/ReVis)
 
-## TODO check the CAFE5 input file to see what difference it makes!! 
 CAFE_RES=/Users/milena/work/PhD_code/ReVis/example_data/CAFE5_Base_family_results.txt
 OF_RES=/Users/milena/work/PhD_code/ReVis/example_data/N0.tsv
 
 U_NAME=miltr339
 
+for SPECIES in A_verrucosus C_chinensis C_septempunctata D_melanogaster D_ponderosae I_luminosus P_pyralis R_ferrugineus T_castaneum T_molitor Z_morio #A_obtectus B_siliquastri C_maculatus
+do
+    echo ""
+    echo ""
+    echo "--------------------------------------------------------------------------------------------------------------"
+    echo "-------------------------------------------${SPECIES}-------------------------------------------------------"
+    echo "--------------------------------------------------------------------------------------------------------------"
+
+    python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
+        --compute_tables_from_list \
+        --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
+        --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/${SPECIES}_masking.ori.out \
+        --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/${SPECIES}_braker_isoform_filtered.gff \
+        --all_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_all_transcripts_${SPECIES}.txt \
+        --sig_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_sig_transcripts_${SPECIES}.txt \
+        --species_name ${SPECIES} \
+        --bp 10000 \
+        --verbose
+done   
+
+# ## do the plotting based on existing tables
+# 
+# REP_TABLES=/Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables
+# SPECIES=C_maculatus
+# 
 # python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
-#     --compute_tables_from_list \
+#     --plot \
 #     --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-#     --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/B_siliquastri_masking.ori.out \
-#     --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/B_siliquastri_braker_isoform_filtered.gff \
-#     --all_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_all_transcripts_B_siliquastri.txt \
-#     --sig_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_sig_transcripts_B_siliquastri.txt \
-#     --species_name B_siliquastri \
-#     --bp 10000 \
-#     --GF_size_percentile 90 \
+#     --all_before_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_before_all_transcripts.txt \
+#     --sig_before_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_before_sig_transcripts_90th_GF_size_percentile.txt \
+#     --all_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_all_transcripts.txt \
+#     --sig_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_sig_transcripts_90th_GF_size_percentile.txt \
+#     --num_transcripts ${REP_TABLES}/${SPECIES}_transcript_numbers.txt \
+#     --species_name $SPECIES \
 #     --verbose
+# ##
 
-# python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
-#     --compute_tables_from_list \
-#     --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-#     --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/C_maculatus_superscaffolded_masking.ori.out \
-#     --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/C_maculatus_superscaffolded_annotation_isoform_filtered.gff \
-#     --all_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_all_transcripts_C_maculatus.txt \
-#     --sig_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_sig_transcripts_C_maculatus.txt \
-#     --species_name C_maculatus \
-#     --bp 10000 \
-#     --GF_size_percentile 90 \
-#     --verbose
 
-# python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
-#     --compute_tables_from_list \
-#     --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-#     --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/A_obtectus_masking.ori.out \
-#     --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/A_obtectus_braker_isoform_filtered.gff \
-#     --all_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_all_transcripts_A_obtectus.txt \
-#     --sig_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_sig_transcripts_A_obtectus.txt \
-#     --species_name A_obtectus \
-#     --bp 10000 \
-#     --GF_size_percentile 90 \
-#     --verbose
-
-# python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis.py \
-#     --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/C_maculatus_superscaffolded_masking.ori.out \
-#     --masker_out_gff /Users/${U_NAME}/work/repeatmasking/repeat_gffs/C_maculatus_superscaffolded_assembly_genomic.fna.out.gff \
-#     --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-#     --species_name C_maculatus \
-#     --window_length 3e6 \
-#     --plot_overlap_filtered \
-#     --verbose \
-#     --plot
-
-# python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis.py \
-#     --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/B_siliquastri_masking.ori.out \
-#     --masker_out_gff /Users/${U_NAME}/work/repeatmasking/repeat_gffs/B_siliquastri_assembly_genomic.fna.out.gff \
-#     --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/B_siliquastri_orthoDB_filtered.gff \
-#     --merge_gene_windows 2 \
-#     --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-#     --species_name B_siliquastri \
-#     --window_length 1e6 \
-#     --plot_overlap_filtered \
-#     --verbose \
-#     --plot
-
-REP_TABLES=/Users/miltr339/work/PhD_code/PhD_chapter1/data/repeats_tables
-SPECIES=C_maculatus
-
-python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
-    --plot \
-    --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-    --all_before_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_before_all_transcripts.txt \
-    --sig_before_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_before_sig_transcripts_90th_GF_size_percentile.txt \
-    --all_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_all_transcripts.txt \
-    --sig_after_table ${REP_TABLES}/${SPECIES}_cumulative_repeats_after_sig_transcripts_90th_GF_size_percentile.txt \
-    --num_transcripts ${REP_TABLES}/${SPECIES}_transcript_numbers.txt \
-    --species_name $SPECIES \
-    --verbose
+### I can't do the repeat histograms in a loop because they all require different window lengths
