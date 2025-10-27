593bfeaf9eaf5c324cd14afb7186a980ecfb67cc
milena-t <milena.trabert@t-online.de>
2025-10-27 14:46:56 +0100

    test .out vs ori.out

--- bash_scripts/plot_all_transcript_surroundings.sh
+++ bash_scripts/plot_all_transcript_surroundings.sh
@@ -7,7 +7,7 @@ OF_RES=/Users/milena/work/PhD_code/ReVis/example_data/N0.tsv
 
 U_NAME=miltr339
 
-for SPECIES in A_verrucosus C_chinensis C_septempunctata D_melanogaster D_ponderosae I_luminosus P_pyralis R_ferrugineus T_castaneum T_molitor Z_morio #A_obtectus B_siliquastri C_maculatus
+for SPECIES in A_obtectus A_verrucosus C_chinensis C_maculatus C_septempunctata D_melanogaster D_ponderosae I_luminosus P_pyralis R_ferrugineus T_castaneum T_molitor Z_morio # B_siliquastri 
 do
     echo ""
     echo ""
@@ -18,7 +18,7 @@ do
     python3 /Users/${U_NAME}/work/PhD_code/ReVis/src/ReVis/ReVis_transcript_surroundings.py \
         --compute_tables_from_list \
         --out_dir /Users/${U_NAME}/work/PhD_code/PhD_chapter1/data/repeats_tables \
-        --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/${SPECIES}_masking.ori.out \
+        --masker_outfile /Users/${U_NAME}/work/repeatmasking/repeat_gffs/${SPECIES}_repeats.out \
         --annotation_gff /Users/${U_NAME}/work/orthoDB_annotations/${SPECIES}_braker_isoform_filtered.gff \
         --all_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_all_transcripts_${SPECIES}.txt \
         --sig_list /Users/miltr339/work/PhD_code/PhD_chapter1/data/CAFE_convergence/overlap_sig_transcripts_${SPECIES}.txt \
